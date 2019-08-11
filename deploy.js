c58c3354745d5a9135eb319105c67fa373eea4ff
jpribyl <pribylsnbits@gmail.com>
2019-08-11 12:12:42 -0600

    updated deploy script for dev and prod

--- deploy.js
+++ deploy.js
@@ -1,15 +1,21 @@
-var shell = require('shelljs')
+var shell = require('shelljs');
 var ghpages = require('gh-pages');
 
-//shell.exec("npm run build")
-//shell.echo("Project built successfully, publishing to dev")
-//shell.exec("gh-pages -d build")
-//shell.echo("Project published to dev, publishing to prod")
-//shell.exec("git fetch origin gh-pages:gh-pages")
-//shell.exec("git push -f prod gh-pages:master")
-//
-ghpages.publish('build', {
-  add: true,
-  branch: "master",
-  repo: 'git@github.com:jpribyl/jpribyl.github.io.git'
+shell.exec("npm run build")
+process.argv.forEach(function(val, index, array) {
+  if (val === 'dev') {
+    ghpages.publish('build', {
+      branch: 'gh-pages',
+      repo: 'git@github.com:jpribyl/website-frontend-dev.git'
+    });
+    shell.echo('Published Dev');
+  }
+  if (val === 'prod') {
+    ghpages.publish('build', {
+      add: true,
+      branch: 'master',
+      repo: 'git@github.com:jpribyl/jpribyl.github.io.git'
+    });
+    shell.echo('Published Prod');
+  }
 });
