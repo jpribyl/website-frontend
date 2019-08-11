9749446ab7866091a5dcacb04eec321b79583aab
jpribyl <pribylsnbits@gmail.com>
2019-08-11 07:22:58 -0600

    updated deploy to be more verbose

--- deploy.js
+++ deploy.js
@@ -1,6 +1,8 @@
 var shell = require('shelljs')
 
 shell.exec("npm run build")
+shell.echo("Project built successfully, publishing to dev")
 shell.exec("gh-pages -d build")
+shell.echo("Project published to dev, publishing to prod")
 shell.exec("git fetch origin gh-pages:gh-pages")
 shell.exec("git push prod gh-pages:master")
